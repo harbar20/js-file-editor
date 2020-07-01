7a879b1449a2ed2f86e76e7a57238841f7b722d7
harbar20 <harbar2021@gmail.com>
2020-07-01 11:14:35 -0400

    cleaned repo again

--- AbilityAdder.py
+++
@@ -1,6 +0,0 @@
-class AbilityAdder():
-    def __init__(self, description, shortDesc, name, rating):
-        self.name = name
-        self.desc = description
-        self.shortDesc = shortDesc
-        self.rating = rating
